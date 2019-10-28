c4922ae2ef942719f1b52de34b7be191308f2408
AnastasiaSD <32881045+AnastasiaSD@users.noreply.github.com>
2019-10-28 17:01:23 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,138 @@
+using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bmw = new Car(new BmwFactory());
+            bmw.GetNameBody();
+            bmw.GetNameMotor();
+            bmw.GetNameCabin();
+
+            var audi = new Car(new AudiFactory());
+            audi.GetNameBody();
+            audi.GetNameMotor();
+            audi.GetNameCabin();
+        }
+    }
+
+    #region Interfaces
+
+    public interface ICarFactory
+    {
+        IBody CreateBody();
+        IMotor CreateMotor();
+        ICabin CreateCabin();
+    }
+
+    public interface IBody
+    {
+        string Name { get; }
+    }
+
+    public interface IMotor
+    {
+        string Name { get; }
+    }
+
+    public interface ICabin
+    {
+        string Name { get; }
+    }
+
+    #endregion
+
+    #region BMW Car
+
+    public class BmwFactory : ICarFactory
+    {
+        public IBody CreateBody()
+        {
+            return new BmwBody();
+        }
+
+        public IMotor CreateMotor()
+        {
+            return new BmwMotor();
+        }
+
+        public ICabin CreateCabin()
+        {
+            return new BmwCabin();
+        }
+    }
+
+    public class BmwBody : IBody
+    {
+        public string Name => "Кузов BMW";
+    }
+
+    public class BmwMotor : IMotor
+    {
+        public string Name => "Мотор BMW";
+    }
+
+    public class BmwCabin : ICabin
+    {
+        public string Name => "Салон BMW";
+    }
+
+    #endregion
+
+    #region AUDI Car
+
+    public class AudiFactory : ICarFactory
+    {
+        public IBody CreateBody()
+        {
+            return new AudiBody();
+        }
+
+        public IMotor CreateMotor()
+        {
+            return new AudiMotor();
+        }
+
+        public ICabin CreateCabin()
+        {
+            return new AudiCabin();
+        }
+    }
+
+    public class AudiBody : IBody
+    {
+        public string Name => "Кузов AUDI";
+    }
+
+    public class AudiMotor : IMotor
+    {
+        public string Name => "Мотор AUDI";
+    }
+
+    public class AudiCabin : ICabin
+    {
+        public string Name => "Салон AUDI";
+    }
+
+    #endregion
+
+    public class Car
+    {
+        private IBody body;
+        private IMotor motor;
+        private ICabin cabin;
+
+        public Car(ICarFactory factory)
+        {
+            body = factory.CreateBody();
+            motor = factory.CreateMotor();
+            cabin = factory.CreateCabin();
+        }
+
+        public void GetNameBody() => Console.WriteLine(body.Name);
+        public void GetNameMotor() => Console.WriteLine(motor.Name);
+        public void GetNameCabin() => Console.WriteLine(cabin.Name);
+    }
+}
